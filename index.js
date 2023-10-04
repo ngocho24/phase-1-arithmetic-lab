0f4d38cca5ab1d8d66eab878395a98376ea7e8f3
Elijah Ngocho <elijahngocho24@gmail.com>
2023-10-04 09:45:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// an equation whose multiplied results will equal 62
+const numb1 = 1;
+const numb2 = 62;
+const multiply = numb1 * numb2;
+console.log (multiply);
+
+// generates a random integer greater than 0
+const random = Math.floor(Math.random())+1;
+ console.log(random);
+
+//  an equation that calculates a remainder that is equal to 4
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(19, 20);
+console.log(max);
